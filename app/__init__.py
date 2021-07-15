a17749c1b7eb6b1d0ad2f9578f65fd506c919486
Alan Telles <alantelles86@gmail.com>
2021-07-15 00:02:01 -0300

    ajustes para utilizacao de chave como variavel de ambiente

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
-import os
+import os, base64
 
 db_engine = os.environ.get('DB_ENGINE')
 
 if db_engine == 'FireBaseDbAccess':
     from app.services.db_access.firebase import FireBaseDbAccess
+    firestore_key = os.environ.get('FIRESTORE_B64')
+    firestore_key_path = os.environ.get('FIRESTORE_KEY')
+    if firestore_key and firestore_key_path:
+        with open(firestore_key_path, 'wt') as fs_handler:
+            decoded = base64.b64decode(firestore_key)
+            fs_handler.write(decoded.decode())
+
     db = FireBaseDbAccess()
 
 else:
